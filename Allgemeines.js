bd62e6f07b5ad1b9d81cff08b481ee08a37e5b58
Daniel Ishi <daniel.bratschke@gmail.com>
2012-01-06 15:43:15 +0100

    Update Allgemeines.js

--- Allgemeines.js
+++ Allgemeines.js
@@ -1,9 +1,13 @@
-
 function Farbe (R, G, B) {
   this.R = R;
   this.G = G;
   this.B = B;
-  this.hex = "#";
-  function gibFarbe(){ alert  ('hallo'); }
+  this.hex = "#"; 
 
 }
+
+Farbe.prototype.gibFarbe = function()
+    {
+            return "hihihihi";
+    }
+
