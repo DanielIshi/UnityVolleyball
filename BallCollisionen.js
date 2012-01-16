b15cf54addabb25dc2fb3e462824477f84e06152
Daniel Ishi <daniel.bratschke@gmail.com>
2012-01-16 15:16:33 +0100

    Update BallCollisionen.js

--- BallCollisionen.js
+++ BallCollisionen.js
@@ -1,17 +1,42 @@
 function OnCollisionEnter(collision : Collision) {
-      
+
       if (G.Phase=="Annahme" ) {
-            if (other.gameObject.name == "Ball" && antizipierter Sektor=="vorne_oben") {
+            if (other.gameObject.name == "Ball" && antizipierter Sektor=="vorne_oben_li") {
                   G.AnnahmeErfolgreich=true;
                   G.letztePhaseAbgeschlossen=true;
             }
-      }
-      
-      
-      // Debug-draw all contact points and normals
-      for (var contact : ContactPoint in collision.contacts) {
-            Debug.DrawRay(contact.point, contact.normal, Color.white);
-      }
-      
-     
+            if (other.gameObject.name == "Ball" && antizipierter Sektor=="vorne_oben_re") {
+                  G.AnnahmeErfolgreich=true;
+                  G.letztePhaseAbgeschlossen=true;
+            }
+            if (other.gameObject.name == "Ball" && antizipierter Sektor=="vorne_unten_re") {
+                  G.AnnahmeErfolgreich=true;
+                  G.letztePhaseAbgeschlossen=true;
+            }
+            if (other.gameObject.name == "Ball" && antizipierter Sektor=="vorne_unten_li") {
+                  G.AnnahmeErfolgreich=true;
+                  G.letztePhaseAbgeschlossen=true;
+            }
+             if (other.gameObject.name == "Ball" && antizipierter Sektor=="hinten_oben_li") {
+                  G.AnnahmeErfolgreich=true;
+                  G.letztePhaseAbgeschlossen=true;
+            }
+            if (other.gameObject.name == "Ball" && antizipierter Sektor=="hinten_oben_re") {
+                  G.AnnahmeErfolgreich=true;
+                  G.letztePhaseAbgeschlossen=true;
+            }
+            if (other.gameObject.name == "Ball" && antizipierter Sektor=="hinten_unten_re") {
+                  G.AnnahmeErfolgreich=true;
+                  G.letztePhaseAbgeschlossen=true;
+            }
+            if (other.gameObject.name == "Ball" && antizipierter Sektor=="hinten_unten_li") {
+                  G.AnnahmeErfolgreich=true;
+                  G.letztePhaseAbgeschlossen=true;
+            }
+            
+            
+            
+            
+      }    
+    
 }
