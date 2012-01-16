91654942a7077a604dd9765e9dfd5b8147a8bdfe
Daniel Ishi <daniel.bratschke@gmail.com>
2012-01-16 10:50:52 +0100

    Update TastaturController.js

--- TastaturController.js
+++ TastaturController.js
@@ -39,32 +39,38 @@ function Update () {
   if (G.SpielLaueft==true) {
 	if (G.letztePhaseAbgeschlossen==true) {
 
-		if (G.Phase=="AnnahmeWartend") {}
-		if (G.Phase=="Annahme") {}
-		if (G.Phase=="Zuspiel") {}
-		if (G.Phase=="Angriff") {}
+		if (G.Phase=="AnnahmeWartend") {AnnahmeVorbereitungTasten();}
+		if (G.Phase=="Annahme") {AnnahmeTasten();}
+		if (G.Phase=="Zuspiel") {ZuspielTasten();}
+		if (G.Phase=="Angriff") {AngriffTasten();}
 		if (G.Phase=="Angriffssicherung") {}
 		if (G.Phase=="UebergangZumK2"){}
-		if (G.Phase=="Blockvorbereitung") {}
-		if (G.Phase=="Blockhandlung") {}
-		if (G.Phase=="Feldabwehr") {}
+		if (G.Phase=="Blockvorbereitung") {BlockVorbereitungTasten();}
+		if (G.Phase=="Blockhandlung") {BlockTasten();}
+		if (G.Phase=="Feldabwehr") {AbwehrTasten();}
 
-		G.letztePhaseAbgeschlossen=false;
+	
 	}
         
     }
-else {
-if (
-	G.letztenSpielzugGewonnen==true){G.aktSpielphase="K2";}else {G.aktSpielphase="K1";}
 
-        }
-  
   
   
   
 }
 
 
+
+
+
+
+
+
+
+
+function AnnahmeVorbereitungTasten()	{ 	NormalLinksRechtsObenUnten();}	
+function BlockVorbereitungTasten()	{  	NormalLinksRechts(); }
+function AufschlagVorbereitung() 	{	NormalLinksRechtsObenUnten();}
 
 function AngriffTasten(){
 	if(Input.GetKeyDown("space"))  		{ 	StemmschrittBeginnen ();  }
@@ -73,13 +79,16 @@ function AngriffTasten(){
 	if(Input.GetKeyDown("backspace")) 	{ 	Lobschlag ();  }
 }
 function AnnahmeTasten(){
+	RichtungFeststellen();
 	if(Input.GetKeyDown("space"))  		{ 	AnnahmeImOberenZuspiel ();  }
 	if(Input.GetKeyDown("return")) 		{ 	Baggern ();  }
 	if(Input.GetKeyDown("right alt")) 	{ 	Rutschbagger ();  }	
 	
 }
 function BlockTasten(){
-	if(Input.GetKeyDown("space"))  		{ 	Blockabsprung ();  }
+	if(Input.GetKeyDown("space"))  		{ 	Blockabsprung ();  }  
+	// if (Input.GetButton ("Jump")) { moveDirection.y = jumpSpeed;}
+ 	//moveDirection.y -= gravity * Time.deltaTime;   	// Apply gravity
 	if(Input.GetKeyDown("return")) 		{ 	AktivBlocken ();  }
 	if(Input.GetKeyDown("right alt")) 	{ 	PassivBlocken ();  }
 }
@@ -94,4 +103,44 @@ function ZuspielTasten(){
 	if(Input.GetKeyDown("space"))  		{ 	ZweitesTempoZuspiel ();  }
 	if(Input.GetKeyDown("return")) 		{ 	DrittesTempoZuspiel  ();  }
 	if(Input.GetKeyDown("right alt")) 	{ 	Hinterfeldpass ();  }
+}
+function AbwehrTasten(){
+	if(Input.GetKeyDown("left alt")) 	{ 	Fallen ();  }
+	if(Input.GetKeyDown("space"))  		{ 	Handabwehr ();  }
+	if(Input.GetKeyDown("return")) 		{ 	Abstuetzen  ();  }
+}
+
+
+
+
+
+
+
+
+////______________________________________/////
+
+var speed : float = 6.0;
+var jumpSpeed : float = 8.0;
+var gravity : float = 20.0;
+private var moveDirection : Vector3 = Vector3.zero;
+
+
+function NormalLinksRechts(){
+	var controller : CharacterController = GetComponent(CharacterController);
+	if (controller.isGrounded) {		
+		moveDirection = Vector3(Input.GetAxis("Horizontal"), 0,	0);  // We are grounded, so recalculate // move direction directly from axes
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+	}		
+	controller.Move(moveDirection * Time.deltaTime);  	// Move the controller	
+}
+
+function NormalLinksRechtsObenUnten(){
+	var controller : CharacterController = GetComponent(CharacterController);
+	if (controller.isGrounded) {		
+		moveDirection = Vector3(Input.GetAxis("Horizontal"), 0,	Input.GetAxis("Vertical")); // We are grounded, so recalculate // move direction directly from axes
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+	}	
+	controller.Move(moveDirection * Time.deltaTime);  	// Move the controller	
 }
