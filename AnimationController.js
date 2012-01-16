5fd9add79c1f3691f0699d3c20602da475383bd9
Daniel Ishi <daniel.bratschke@gmail.com>
2012-01-16 11:17:12 +0100

    Update AnimationController.js

---
+++ AnimationController.js
@@ -0,0 +1,22 @@
+function StemmschrittBeginnen () {}
+function Schlag () {}
+function Finte () {}
+function Lobschlag () {}
+function RichtungFeststellen() {}
+function AnnahmeImOberenZuspiel () {}
+function Baggern () {} 
+function Rutschbagger () {}
+function Blockabsprung () {}
+function AktivBlocken ()  {}
+function PassivBlocken () {}
+function AufschlagArtUmschalten () {}
+function Anwurf () {}
+function HarterAufSchlag () {}
+function DriveAufschlag ()  {} 
+function ErstesTempoZuspiel () {}
+function ZweitesTempoZuspiel () {}
+function DrittesTempoZuspiel() {}
+function Hinterfeldpass () {}
+function Fallen() {}
+function Handabwehr() {}
+function Abstuetzen() {}
